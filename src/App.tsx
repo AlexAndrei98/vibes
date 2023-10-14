26e369776a72a6e6bdf2bbe990d249086f78fc3c
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-10-14 19:03:54 -0400

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,19 @@ interface User {
 
 }
 
-// Global data structure
-let usersData: User[] = [];
-
-
-const LogInPage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateAction<string>>, setLoggedInUser: React.Dispatch<React.SetStateAction<User | undefined>> }> = ({ setActiveComponent, setLoggedInUser }) => {
+const LogInPage: React.FC<{
+  setActiveComponent: React.Dispatch<React.SetStateAction<string>>,
+  setLoggedInUser: React.Dispatch<React.SetStateAction<User | undefined>>,
+  usersData: User[],
+  setUsersData: React.Dispatch<React.SetStateAction<User[]>>
+}> = ({ setActiveComponent, setLoggedInUser, usersData }) => {
   const [enteredCode, setEnteredCode] = useState<number>();
 
   const handleLogin = () => {
-    if (usersData.some(user => user.code === enteredCode)) {
+    const foundUser = usersData.find(user => user.code === enteredCode);
+    if (foundUser) {
+      setLoggedInUser(foundUser);
       setActiveComponent('leaderboard');
-      setLoggedInUser(usersData.find(user => user.code === enteredCode));
     } else {
       message.error('Invalid code. Please try again.');
     }
@@ -48,8 +50,12 @@ const LogInPage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateAct
     </div>
   );
 }
+const Leaderboard: React.FC<{
+  loggedInUser?: User,
+  setLoggedInUser: React.Dispatch<React.SetStateAction<User | undefined>>,
+  usersData: User[]
+}> = ({ loggedInUser, setLoggedInUser, usersData }) => {
 
-const Leaderboard: React.FC<{ loggedInUser?: User, setLoggedInUser: React.Dispatch<React.SetStateAction<User | undefined>> }> = ({ loggedInUser, setLoggedInUser }) => {
   function getWeekNumber(d: Date) {
     const date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     const dayNum = date.getUTCDay() || 7;
@@ -64,53 +70,48 @@ const Leaderboard: React.FC<{ loggedInUser?: User, setLoggedInUser: React.Dispat
   const handleClick = (direction: string, targetUser: User) => {
     if (!loggedInUser) return;
     const currentWeek = getWeekNumber(new Date());
-    const loggedInUserIndex = usersData.findIndex(u => u.name === loggedInUser.name);
-    const targetUserIndex = usersData.findIndex(u => u.name === targetUser.name);
-
-    if (!usersData[loggedInUserIndex].actionsTaken) {
-      usersData[loggedInUserIndex].actionsTaken = {};
+    const updatedUsersData = [...usersData];
+    const loggedInUserIndex = updatedUsersData.findIndex(u => u.name === loggedInUser.name);
+    const targetUserIndex = updatedUsersData.findIndex(u => u.name === targetUser.name);
+    if (!updatedUsersData[loggedInUserIndex].actionsTaken) {
+      updatedUsersData[loggedInUserIndex].actionsTaken = {};
     }
-
-    if (!usersData[loggedInUserIndex].actionsTaken[currentWeek]) {
-      usersData[loggedInUserIndex].actionsTaken[currentWeek] = {};
+    // @ts-ignore: Object is possibly 'undefined'
+    if (!updatedUsersData[loggedInUserIndex].actionsTaken[currentWeek]) {
+      // @ts-ignore: Object is possibly 'undefined'
+      updatedUsersData[loggedInUserIndex].actionsTaken[currentWeek] = {};
     }
-
-
-    //check if the user has already taken this specific action on this user for this week and if they have reset it and the score
     if (selectedActions[targetUser.name] === direction) {
       if (direction === "home") {
-        usersData[targetUserIndex].score += 1;
+        updatedUsersData[targetUserIndex].score += 1;
       } else if (direction === "moon") {
-        usersData[targetUserIndex].score -= 1;
+        updatedUsersData[targetUserIndex].score -= 1;
       }
-
-      delete usersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name];
+      // @ts-ignore: Object is possibly 'undefined'
+      delete updatedUsersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name];
       loggedInUser.weeks.find(w => w.week === currentWeek)!.count += 1;
       const updatedActions = { ...selectedActions };
       delete updatedActions[targetUser.name];
       setSelectedActions(updatedActions);
       return;
     }
-
-    if (usersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name]) {
+    // @ts-ignore: Object is possibly 'undefined'
+    if (updatedUsersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name]) {
       message.error('You have already taken action on this user for this week.');
       return;
     }
-
     if (loggedInUser.weeks.find(w => w.week === currentWeek)!.count === 0) {
       message.error('You have already taken action on 5 users for this week.');
       return;
     }
-
     if (direction === "home") {
-      usersData[targetUserIndex].score -= 1;
+      updatedUsersData[targetUserIndex].score -= 1;
     } else if (direction === "moon") {
-      usersData[targetUserIndex].score += 1;
+      updatedUsersData[targetUserIndex].score += 1;
     }
-
-    usersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name] = true;
+    // @ts-ignore: Object is possibly 'undefined'
+    updatedUsersData[loggedInUserIndex].actionsTaken[currentWeek][targetUser.name] = true;
     loggedInUser.weeks.find(w => w.week === currentWeek)!.count -= 1;
-
     if (selectedActions[targetUser.name] === direction) {
       const updatedActions = { ...selectedActions };
       delete updatedActions[targetUser.name];
@@ -119,81 +120,82 @@ const Leaderboard: React.FC<{ loggedInUser?: User, setLoggedInUser: React.Dispat
       setSelectedActions({ ...selectedActions, [targetUser.name]: direction });
     }
   };
-
   const handleConfirm = () => {
     if (!loggedInUser) return;
-
     const currentWeek = getWeekNumber(new Date());
-
     let w = loggedInUser.weeks.find(w => w.week === currentWeek);
     if (w) {
       w.count -= 1;
     }
-    console.log(loggedInUser);
     setLoggedInUser({ ...loggedInUser });
   };
 
-
+  //sort the usersData by score
+  let _userData = [...usersData]
+  let _sortedUserData = _userData.sort((a, b) => b.score - a.score);
+  //write some code that picks a hue between red and green based on the score
 
 
   return (
     <div className='leaderboard'>
       <h2>Welcome {loggedInUser?.name}!</h2>
-      {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count < 0 &&
-        <h2> You have no votes left</h2>}
-      {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count >= 0 &&
-        <h2> You have {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count} votes left</h2>}      {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count > -1 &&
-          <div className="leaderboard-content">
-            {usersData.map(user => (
-              <div className="user-card" key={user.name}>
-                <div className="avatar-placeholder">{user.name.charAt(0)}</div>
-                <div className="user-details">
-                  <span>{user.name}</span>
-                </div>
-                <div className="action-buttons">
-                  <Button
-                    type={selectedActions[user.name] === 'home' ? 'primary' : 'default'}
-                    disabled={selectedActions[user.name] === 'moon'}
-                    onClick={() => handleClick('home', user)}>Going Home</Button>
-                  <Button
-                    type={selectedActions[user.name] === 'moon' ? 'primary' : 'default'}
-                    disabled={selectedActions[user.name] === 'home'}
-                    onClick={() => handleClick('moon', user)}>To The Moon</Button>
-                </div>
-              </div>
-            ))}
-            <Button type="primary"
+      {loggedInUser!.weeks.find(w => w.week === currentWeek)!.count < 0 &&
+        <h2> Thank you for voting this week.</h2>}
+      {loggedInUser!.weeks.find(w => w.week === currentWeek)!.count >= 0 &&
+        <h2> You have {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count} votes left</h2>}
 
-              disabled={loggedInUser?.weeks.find(w => w.week === currentWeek)!.count != 0}
-
-              onClick={handleConfirm}
-
-            >
-              Confirm</Button>
-
-          </div>}
-      {loggedInUser?.weeks.find(w => w.week === currentWeek)!.count < 0 &&
+      {loggedInUser!.weeks.find(w => w.week === currentWeek)!.count > -1 &&
         <div className="leaderboard-content">
-          <h2>Leaderboard</h2>
-          <body>{JSON.stringify(usersData,null,2)}</body>
           {usersData.map(user => (
             <div className="user-card" key={user.name}>
               <div className="avatar-placeholder">{user.name.charAt(0)}</div>
               <div className="user-details">
                 <span>{user.name}</span>
               </div>
-              <div className="user-details">
-                <span>Score: {user.score}</span>
+              <div className="action-buttons">
+                <Button
+                  type={selectedActions[user.name] === 'home' ? 'primary' : 'default'}
+                  disabled={selectedActions[user.name] === 'moon'}
+                  onClick={() => handleClick('home', user)}>Going Home</Button>
+                <Button
+                  type={selectedActions[user.name] === 'moon' ? 'primary' : 'default'}
+                  disabled={selectedActions[user.name] === 'home'}
+                  onClick={() => handleClick('moon', user)}>To The Moon</Button>
               </div>
             </div>
           ))}
+          <Button type="primary"
+            disabled={loggedInUser?.weeks.find(w => w.week === currentWeek)!.count != 0}
+            onClick={handleConfirm}>
+            Confirm</Button>
+        </div>}
+      {loggedInUser!.weeks.find(w => w.week === currentWeek)!.count === -1 &&
+        <div>
+          <h2>Leaderboard</h2>
+          <div className="leaderboard-content">
+            {_sortedUserData.map(user => (
+              <div className="user-card" key={user.name}>
+                <div className="avatar-placeholder">{user.name.charAt(0)}</div>
+                <div className="user-details">
+                  <span>{user.name}</span>
+                </div>
+                <div className="user-details">
+                  <span>Score: {user.score}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>}
     </div>
+
   );
 };
 
-
-const HomePage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateAction<string>> }> = ({ setActiveComponent }) => {
+const HomePage: React.FC<{
+  setActiveComponent: React.Dispatch<React.SetStateAction<string>>,
+  usersData: User[],
+  setUsersData: React.Dispatch<React.SetStateAction<User[]>>
+}> = ({ setActiveComponent, usersData, setUsersData }) => {
   const [selectedName, setSelectedName] = useState<string | undefined>(undefined);
   const [randomNumber, setRandomNumber] = useState<number | undefined>(undefined);
 
@@ -206,6 +208,7 @@ const HomePage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateActi
     const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
     return Math.ceil((((date.valueOf() - yearStart.valueOf()) / 86400000) + 1) / 7);
   }
+
   const handleNameSelect = (name: string) => {
     setSelectedName(name);
     const generatedNumber = Math.floor(1000 + Math.random() * 9000);  // This will generate a number between 1000 to 9999
@@ -216,21 +219,25 @@ const HomePage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateActi
     const currentWeek = getWeekNumber(new Date()) // getting the current date as YYYY-MM-DD
     const userIndex = usersData.findIndex(user => user.name === selectedName);
 
+    const updatedUsersData = [...usersData];
+
     if (userIndex === -1) {
-      usersData.push({
+      updatedUsersData.push({
         name: selectedName!,
         code: randomNumber!,
         score: 0,
         weeks: [{ week: currentWeek, count: 2 }]
       });
     } else {
-      usersData[userIndex].weeks.push({ week: currentWeek, count: 2 });
+      updatedUsersData[userIndex].weeks.push({ week: currentWeek, count: 2 });
     }
+
+    setUsersData(updatedUsersData);
 
     // Reset states after claim
     setSelectedName(undefined);
     setRandomNumber(undefined);
-    setActiveComponent('')
+    setActiveComponent('login')
   }
 
   return (
@@ -253,23 +260,40 @@ const HomePage: React.FC<{ setActiveComponent: React.Dispatch<React.SetStateActi
   );
 }
 
+
 const App: React.FC = () => {
   const [activeComponent, setActiveComponent] = useState<string>("");
   const [loggedInUser, setLoggedInUser] = useState<User | undefined>();
   const [usersData, setUsersData] = useState<User[]>([]);
 
+  const handleMenuClick = (page: string) => {
+    //if page is logout the set logged in user to undefined and redirect to login
+    //else setActiveComponent
+
+    if (page === 'logout') {
+      setLoggedInUser(undefined);
+      setActiveComponent('login');
+    }
+    else {
+      setActiveComponent(page);
+    }
+  }
+
   return (
     <Layout className="layout">
       <Header>
-        <Menu theme="dark" mode="horizontal" onClick={({ key }) => setActiveComponent(key.toString())}>
-          <Menu.Item key="login" icon={<LoginOutlined />}>Log In</Menu.Item>
-          <Menu.Item key="leaderboard" icon={<TrophyOutlined />}>Leaderboard</Menu.Item>
+        <Menu theme="dark" mode="horizontal" onClick={({ key }) => handleMenuClick(key.toString())}>
+          {!loggedInUser && <Menu.Item key="homepage" icon={<TrophyOutlined />}>Homepage</Menu.Item>}
+          {loggedInUser && <Menu.Item key="leaderboard" icon={<TrophyOutlined />}>Leaderboard</Menu.Item>}
+          {!loggedInUser && <Menu.Item key="login" icon={<LoginOutlined />}>Log In</Menu.Item>}
+          {loggedInUser && <Menu.Item key="logout" icon={<LoginOutlined />}>Log out</Menu.Item>}
+
         </Menu>
       </Header>
       <Content style={{ padding: '50px' }}>
-        {activeComponent === 'login' && <LogInPage setActiveComponent={setActiveComponent} setLoggedInUser={setLoggedInUser} />}
-        {activeComponent === 'leaderboard' && <Leaderboard loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />}
-        {activeComponent === '' && <HomePage setActiveComponent={setActiveComponent} />}
+        {activeComponent === 'login' && <LogInPage setActiveComponent={setActiveComponent} setLoggedInUser={setLoggedInUser} usersData={usersData} setUsersData={setUsersData} />}
+        {activeComponent === 'leaderboard' && <Leaderboard loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} usersData={usersData} />}
+        {activeComponent === 'homepage' && <HomePage setActiveComponent={setActiveComponent} usersData={usersData} setUsersData={setUsersData} />}
       </Content>
     </Layout>
   );
